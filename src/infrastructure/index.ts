a7d783a16e994e334e45fb11bea8b48772c2bc58
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2020-04-07 13:45:39 -0300

    Configure index files for infrastructure layer

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -1,9 +1,14 @@
 import { init as setUpDatabaseProvider } from './typeOrm'
 import { init as setUpEmailService } from './sendGrid'
 import { init as setUpCacheService } from './redis'
+
 import { AccountRepository } from "../domain/account"
+import { PermissionRepository } from '../domain/permission'
+import { RoleRepository } from '../domain/role'
 
 export let accountRepository: AccountRepository
+export let permissionRepository: PermissionRepository
+export let roleRepository: RoleRepository
 
 export function init() {
   return Promise.all([ 
@@ -13,8 +18,11 @@ export function init() {
 
   ]).then(([ repositories, emailServiceImplementation, cacheServiceImplementation ]) => {
     // Repositories
-    accountRepository = repositories.accountRepositoryImplementation
-    // Return infrastructure instances 
+    accountRepository = repositories.accountRepositoryImplementation,
+    permissionRepository = repositories.permissionRepositoryImplementation,
+    roleRepository = repositories.roleRepositoryImplementation
+
+    // Return infrastructure instances
     return [ repositories, emailServiceImplementation, cacheServiceImplementation ] 
   })
 }
--- src/infrastructure/typeOrm/index.ts
+++ src/infrastructure/typeOrm/index.ts
@@ -1,10 +1,15 @@
 import "reflect-metadata";
-import {createConnection, getCustomRepository } from "typeorm"
+import {createConnection, getConnectionOptions } from "typeorm"
 import { AccountRepositoryImplementation } from './repositories/AccountRepositoryImplementation'
+import { PermissionRepositoryImplementation } from './repositories/PermissionRepositoryImplementation'
+import { RoleRepositoryImplementation } from './repositories/RoleRepositoryImplementation'
 
 export async function init() {
-  await createConnection()
-  return {
-    accountRepositoryImplementation: getCustomRepository(AccountRepositoryImplementation)
-  }
+  return createConnection().then(connection => {
+    return {
+      accountRepositoryImplementation:  connection.manager.getCustomRepository(AccountRepositoryImplementation),
+      permissionRepositoryImplementation:  connection.manager.getCustomRepository(PermissionRepositoryImplementation),
+      roleRepositoryImplementation:  connection.manager.getCustomRepository(RoleRepositoryImplementation)
+    }
+  })
 }
