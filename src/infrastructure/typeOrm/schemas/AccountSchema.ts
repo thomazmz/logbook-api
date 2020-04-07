e6c3d9fc4504fac6a99a2d5584757e061f5ae639
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2020-04-07 13:32:15 -0300

    Define database schema for Account entity

--- src/infrastructure/typeOrm/schemas/AccountSchema.ts
+++ src/infrastructure/typeOrm/schemas/AccountSchema.ts
@@ -5,19 +5,23 @@ export const AccountSchema = new EntitySchema<Account>({
   name: 'account',
   columns: {
     id: {
+      name: 'id',
       type: Number,
       primary: true,
       generated: 'increment'
     },
     email: {
+      name: 'email',
       type: String,
       unique: true
     },
     username: {
+      name: 'username',
       type: String,
       unique: true
     },
     passwordHash: {
+      name: 'password_hash',
       type: String
     }
   },
