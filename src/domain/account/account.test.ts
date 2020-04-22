019523df7c974450cf497d841f9f5bfaa3ebc52e
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2020-04-22 01:08:20 -0300

    Implement Account authentication methods

---
+++ src/domain/account/account.test.ts
@@ -0,0 +1,36 @@
+import jwtDecoder from 'jwt-decode'
+import { Account } from './account'
+
+describe('Account tests', () => {
+
+  it('Should hash account password', async () => {
+    // Given
+    const passwordLiteral = 'password'
+    const account = new Account({})
+    // When
+    await account.setPasswordHash(passwordLiteral)
+    // Then
+    expect(account.passwordHash).not.toBe(passwordLiteral)
+  });
+
+  it('Should verify user password', async () => {
+    // Given
+    const passwordLiteral = 'password'
+    const account = new Account({})
+    // When
+    await account.setPasswordHash(passwordLiteral)
+    // Then
+    expect(account.checkPassword(passwordLiteral)).toBeTruthy()
+    expect(account.checkPassword(`!${passwordLiteral}`)).toBeTruthy()
+  })
+
+  // it('Should generate JWT with user payload', async () => {
+  //   // Given
+  //   const account = new Account({ id: 1 })
+  //   // When
+  //   const jwt = account.generateBarearToken()
+  //   const jwtDecoded = jwtDecoder(jwt)
+  //   // Then
+  //   expect(jwtDecoded.id).toBe(account.id);
+  // })
+})
--- src/domain/account/account.ts
+++ src/domain/account/account.ts
@@ -1,6 +1,10 @@
+import { compare, sign as createToken } from 'jsonwebtoken'
+import { hash } from 'bcrypt';
 import { Role } from '../role'
 
-export default class Account {
+export class Account {
+
+  private static JWT_SECRET = 'vc2iX6e%K>47WVH@#8px9qoYRU@Kes}D8uB4pzzpkZp=L[3RX#$JMtMdVbzv'
 
   id: number
 
@@ -20,7 +24,17 @@ export default class Account {
     Object.assign(this, attributes);
   }
 
-  setPasswordHash (passwordLiteral: string): void {
-    this.passwordHash = passwordLiteral;
+  async setPasswordHash (passwordLiteral: string): Promise<void> {
+    this.passwordHash = await hash(passwordLiteral, 10)
+  }
+
+  async checkPassword(passwordLiteral: string): Promise<boolean> {
+    if(!this.passwordHash) return false;
+    return await compare(passwordLiteral, this.passwordHash)
+  }
+
+  async generateBarearToken() {
+    const payload = { id: this.id }
+    return await createToken(payload, Account.JWT_SECRET)
   }
 }
