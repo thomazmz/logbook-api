82bc62b167501fb429f36aa94cb9f0afd732ea12
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2020-05-09 19:25:24 -0300

    Implement PermissionRepository

--- src/infrastructure/typeOrm/repositories/typeOrmPermissionRepository.test.ts
+++ src/infrastructure/typeOrm/repositories/typeOrmPermissionRepository.test.ts
@@ -4,7 +4,7 @@ import { init as setUpDatabase } from '../initializer'
 import { typeOrmPermissionRepositoryFactory } from './typeOrmPermissionRepository'
 import { Permission, PermissionRepository } from '../../../domain'
 
-describe('Account repository tests', () => {
+describe('Permission repository tests', () => {
 
   let databaseConnection: Connection
   let permissionRepository: PermissionRepository
@@ -18,35 +18,18 @@ describe('Account repository tests', () => {
     await databaseConnection.close()
   })
 
-  it('should assign id when saving new permission', async () => {
-    // Given 
-    const name = `somePermission_${uuid()}`
-    const permission = new Permission({ name })
-    // When
-    await permissionRepository.save(permission)
-    // Then
-    expect(permission.id != null).toBeTruthy()
-  })
-
-  it('should find Permission entity by name', async () => {
-    // Given 
-    const name = `somePermission_${uuid()}`
-    const permission = new Permission({ name })
-    // When
-    await permissionRepository.save(permission)
-    const findedPermission = await permissionRepository.findByName(name)
-    // Then
-    expect(permission.id).toBe(findedPermission.id)
-    expect(permission.name).toBe(findedPermission.name)
-    expect(findedPermission instanceof Permission).toBeTruthy()
-  })
-  
   it('should find or create permission', async () => {
     // Given
     const name = `validPermission_${uuid()}`
+    
     // When
     const permission = await permissionRepository.findOrCreate({ name })
-    const findedPermission = await permissionRepository.findOrCreate({ name} )
+    const findedPermission = await databaseConnection.createQueryBuilder()
+      .select('permission')
+      .from(Permission, 'permission')
+      .where('permission.id = :id', { id: permission.id })
+      .getOne()
+    
     // Then
     expect(findedPermission.id).toBe(permission.id)
     expect(findedPermission.name).toBe(permission.name)
@@ -55,9 +38,15 @@ describe('Account repository tests', () => {
   it('should find or create many permissions', async () => {
     // Given
     const names = [ `someValidPermission_${uuid()}`, `anotherValidPermission_${uuid()}` ]
+    
     // When
     const permissions = await permissionRepository.findOrCreateMany(names.map(name => ({ name })))
-    const findedPermissions = await Promise.all(names.map(name => permissionRepository.findByName(name)))
+    const findedPermissions = await databaseConnection.createQueryBuilder()
+      .select('permission')
+      .from(Permission, 'permission')
+      .where('permission.name IN (:...names)', { names })
+      .getMany()
+    
     // Then
     expect(permissions[0].id).toBe(findedPermissions[0].id)
     expect(permissions[0].name).toBe(findedPermissions[0].name)
@@ -65,16 +54,5 @@ describe('Account repository tests', () => {
     expect(permissions[1].id).toBe(findedPermissions[1].id)
     expect(permissions[1].name).toBe(findedPermissions[1].name)
     expect(permissions[1] instanceof Permission).toBeTruthy()
-  })
-  
-  it('should delete permission by permission name', async () => {
-    // Given
-    const name = `someValidPermission_${uuid()}`
-    await permissionRepository.findOrCreate({ name })
-    // When
-    await permissionRepository.deleteByName(name)
-    const permissions = await permissionRepository.findAll()
-    // Then
-    expect(permissions.some(permission => permission.name == name)).toBeFalsy()
   })
 })
--- src/infrastructure/typeOrm/repositories/typeOrmPermissionRepository.ts
+++ src/infrastructure/typeOrm/repositories/typeOrmPermissionRepository.ts
@@ -7,22 +7,13 @@ import { TypeOrmCrudRepository } from './typeOrmCrudRepository'
 @EntityRepository(PermissionSchema)
 export class TypeOrmPermissionRepository extends TypeOrmCrudRepository<Permission, number> implements PermissionRepository {
 
-  findByName(name: string): Promise<Permission> {
-    return this.repository.findOne({where: { name }})
-  }
-
   findOrCreate(attributes: Partial<Permission>): Promise<Permission> {
     return this.repository.findOne({ where: attributes})
       .then(permission => permission ? permission : this.repository.save(new Permission(attributes)))
   }
-  
+
   findOrCreateMany(attributes: Partial<Permission>[]): Promise<Permission[]> {
     return Promise.all(attributes.map(permissionAttributes => this.findOrCreate(permissionAttributes)))
-  }
-
-  async deleteByName(name: string) {
-    await this.repository.findOne({ where: { name }})
-      .then(permission => this.repository.delete(permission))
   }
 }
 
