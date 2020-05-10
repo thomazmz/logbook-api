bd6a327be3bd5627e031ebc9e6f66709e21749b3
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2020-05-10 14:17:29 -0300

    Change test script to receive more than one path as parameter

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,6 @@ export NODE_ENV=test
 
 npx ts-node ./node_modules/typeorm/cli.js --config ./src/infrastructure/typeOrm/config.ts migration:run
 
-npx jest "$1"
+npx jest "$@"
 
 npx ts-node ./node_modules/typeorm/cli.js --config ./src/infrastructure/typeOrm/config.ts schema:drop
