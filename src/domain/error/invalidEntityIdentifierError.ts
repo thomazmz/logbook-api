a0144ca622e318bdd0efc9a32939ae1b6940a548
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2020-05-19 11:28:22 -0300

    chore(domain): Change errors to accept numbers as "value" parameter argument

--- src/domain/error/invalidEntityIdentifierError.ts
+++ src/domain/error/invalidEntityIdentifierError.ts
@@ -1,7 +1,7 @@
 import { DomainError } from './domainError'
 
 export class InvalidEntityIdentifierError extends DomainError {
-  constructor(domain: string, key: string, value: string, title: string = 'Invalid Entity Identifier') {
+  constructor(domain: string, key: string, value: string | number, title: string = 'Invalid Entity Identifier') {
     super(`${domain} ${key} "${value}" is not valid.`, title)
     Object.setPrototypeOf(this, InvalidEntityIdentifierError.prototype);
   }
--- src/domain/error/unavailableEntityIdentifierError.ts
+++ src/domain/error/unavailableEntityIdentifierError.ts
@@ -1,7 +1,7 @@
 import { DomainError} from './domainError'
 
 export class UnavailableEntityIdentifierError extends DomainError {
-  constructor(domain: string, key: string, value: string, title: string = 'Unavailable Entity Identifier') {
+  constructor(domain: string, key: string, value: string | number, title: string = 'Unavailable Entity Identifier') {
     super(`${domain} ${key} "${value}" is not available.`, title)
     Object.setPrototypeOf(this, UnavailableEntityIdentifierError.prototype);
   }
